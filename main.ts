5c80329b8153642e81f4f0dd41b4cd6010435c27
Samia-irfan <samiairfan00369@gmail.com>
2024-03-30 01:19:52 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,24 @@
+#!/usr/bin/env
+
+import inquirer from "inquirer";
+let todos = [];
+let condition = "true";
+
+while(condition)
+{
+
+let addTask = await inquirer.prompt([{
+    name: "todo",
+    type:"input",
+    message: " What do you want to add in your to do list?"
+},
+{
+    name: "Addmore",
+    type:"confirm",
+    message: "Do you want to increase your todo list?",
+    default:"false"
+}])
+todos.push(addTask.todo);
+condition = addTask.Addmore;
+console.log(todos);
+}
